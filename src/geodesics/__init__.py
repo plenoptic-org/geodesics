ec9c0e288847102f011fb39eb292d5f4112b1cbb
William F. Broderick <billbrod@gmail.com>
2024-08-23 15:54:16 -0400

    adds factorized pyramid

--- src/geodesics/__init__.py
+++ src/geodesics/__init__.py
@@ -2,3 +2,4 @@
 
 from .geodesic import Geodesic, plot_loss, plot_deviation_from_line
 from .straightness import make_straight_line, sample_brownian_bridge, deviation_from_line, translation_sequence
+from .factorized_pyramid import FactorizedPyramid
